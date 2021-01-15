4a8edb0b2afca630eda7662390815bdf3c8a67f1
famous <famous0814@gmail.com>
2021-01-15 10:12:27 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TodoAppAngular';
+  Todo = [];
+  name = '';
+  editable = false;
+  addButton = "Add";
+editableIndex='';
+
+
+  Add(editableIndex){
+    if (this.name == '') return;
+    if(this.editable === true){
+      const TodosList: Todo = {
+        name: this.name,
+        editable: this.editable
+      }
+      this.Todo.splice(editableIndex,1,TodosList);
+      this.addButton = "Add";
+      this.editable = false;
+    }
+    else{
+      const TodosList: Todo = {
+        name: this.name,
+        editable: this.editable
+      }
+      this.Todo.splice(0,0,TodosList);
+      this.name = "";
+    }
+  }
+
+Delete(index){
+ this.Todo.splice(index,1);
+}
+
+Edit(item,index){
+this.editable = true;
+this.addButton = "UPDATE"
+this.name = item;
+this.editableIndex = index;
+}
+  ngOnInit() {
+
+    $(document).ready(function() {
+
+         //alert('we call alert from JQuery');
+
+    });
+
+ }
 }
